a6e1f4bf64f1984a27b7c9cd56dcbfddc58a213b
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2016-01-01 12:09:35 +0200

    Enabled cors in express.

--- server/config/express-config.js
+++ server/config/express-config.js
@@ -2,11 +2,13 @@
 let express = require('express'),
     bodyParser = require('body-parser'),
     ejs = require('ejs'),
+    enableCors = require('./utilities/cors-service'),
     viewsPath = `${__dirname}/../../public/views`;
 
 module.exports = {
     configure: function(app) {
         app.use(bodyParser.json());
+        app.use(enableCors);
         app.set('views', viewsPath);
         app.engine('html', ejs.renderFile);
         app.use(express.static(__dirname + '/../../public/'));
